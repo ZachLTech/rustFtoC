b0337ea1b79cfad2d3a64ee36256a40d2e1a3a69
ZachLTech <zlopezzl2021@gmail.com>
2023-04-02 14:58:56 -0400

    practicing...

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,36 @@ use std::io;
 use std::io::Read;
 
 fn main() {
-    let mut input = String::new();
+    let mut answer = String::new();
     
-    println!("Do you want to Convert (1) Degrees to Celcius or (2) Celcius to Degrees?");
-    io::stdin().read_line(&mut input).expect("Failed to Read Line");
+    println!("Do you want to Convert (1) Fahrenheit to Celcius or (2) Celcius to Fahrenheit?: ");
+    io::stdin().read_line(&mut answer).expect("Failed to Read Line");
 
+    let answer = answer.trim().parse::<i32>().unwrap();
+    if answer == 1{
+        let mut temp = String::new();
+        println!("What's the temperature you want to convert?: ");
+        io::stdin().read_line(&mut temp).expect("Failed to Read Line");
+        let mut temp = temp.trim().parse::<f64>().unwrap();
+        fahrenheit_to_celcius(temp);
+    }
+    else if answer == 2 {
+        let mut temp = String::new();
+        println!("What's the temperature you want to convert?: ");
+        io::stdin().read_line(&mut temp).expect("Failed to Read Line");
+        let mut temp = temp.trim().parse::<f64>().unwrap();
+        celcius_to_fahrenheit(temp);
+    }
+    else {
+        println!("That's Not an option.");
+    }
 }
 
-fn celcius_to_fahrenheit(celcius: f64) -> f64 {
-    celcius * 1.8 + 32.0
+fn celcius_to_fahrenheit(mut celcius: f64) {
+    celcius = celcius * 1.8 + 32.0;
+    println!("Your converted temp is: {celcius}");
 }
-fn fahrenheit_to_celcius(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) / 1.8
+fn fahrenheit_to_celcius(mut fahrenheit: f64) {
+    fahrenheit = (fahrenheit - 32.0) / 1.8;
+    println!("Your converted temp is: {fahrenheit}");
 }
